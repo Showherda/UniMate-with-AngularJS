03681d281ef408d313ff381aba7499903f41fe30
Showherda <showherda@gmail.com>
2022-08-02 13:52:15 +0600

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,51 @@
+var app=angular.module("myApp", ['ngSanitize']);
+app.controller('myCtrl', function($scope, $http) {
+    const requestURL='https://showherda.github.io/unimate/data.txt';
+    $scope.submitButtonDisabled=true;
+    $scope.all=[[]];
+    $scope.costs=[];
+    $scope.numEntryOptions=[10, 25, 50, 100];
+    
+    for (var i=5000; i<85000; i+=5000)
+        $scope.costs.push(i);
+    
+    $http.get(requestURL).then(function (response) {
+        $scope.submitButtonDisabled=false;
+        for (let v of response.data.split(';')){
+			v=v.replace('\n', '');
+			let t=[], i=0, s='';
+			while (i<v.length){
+				if (v[i]===',')
+					$scope.all[$scope.all.length-1].push(s),
+					s='';
+				else if (v[i]==='"'){
+					let j=i+1;
+					while (v[j]!=='"')
+						s+=v[j++];
+					t.push(s);
+					i=j
+				}
+				else
+					s+=v[i];
+				i+=1
+			}
+			$scope.all[$scope.all.length-1].push(s);
+			$scope.all.push([]);
+		}
+		$scope.all.pop();
+        
+        $scope.search = function() {
+            if ($scope.ed)
+                $scope.all.sort((arr) => parseFloat(arr[9])+parseFloat(arr[14])+parseFloat(arr[15])-$scope.efc+(parseFloat(arr[1])==0?1e6:-1e6));
+            else
+                $scope.all.sort((arr) => parseFloat(arr[9])+parseFloat(arr[14])+parseFloat(arr[15])-$scope.efc);
+        }
+
+        $scope.displayInfo = function() {
+            console.log("YES");
+        }
+    });
+});
+
+// 0 name, 1 ed, 2 rd, 3  percent of class from ed, 4 ed to rd, 5 additional admission plans, 6 total cost of attendance
+// 7 full-time ug students, 8 full time intl students, 9 intl to local percentage, 10 need-based, 11 merit, 12 no aid, 13 average finaid, 14 percentage of intl receiving aid, 15 efc
---
+++ script.py
@@ -0,0 +1,56 @@
+import sys
+data=open("data.txt").read()
+sys.stdout=open("output.txt",'w')
+print('[')
+for v in data.split(';'):
+    cnt=0
+    i=0
+    tmp=''
+    v=v.strip('\n')
+    v+=','
+    while i<len(v):
+        if v[i]==',':
+            if cnt==0:
+                print('{name:"'+tmp+'"',end=',')
+            elif cnt==1:
+                print('ed:'+tmp,end=',')
+            elif cnt==2:
+                print('rd:'+tmp,end=',')
+            elif cnt==3:
+                print("edp:"+tmp,end=',')
+            elif cnt==4:
+                print("edtord:"+tmp,end=',')
+            elif cnt==5:
+                print('aap:"'+tmp+'"',end=',')
+            elif cnt==6:
+                print('coa:'+tmp,end=',')
+            elif cnt==7:
+                print('ftug:'+tmp,end=',')
+            elif cnt==8:
+                print('ftinc:'+tmp,end=',')
+            elif cnt==9:
+                print('pinc:'+tmp,end=',')
+            elif cnt==10:
+                print('nb:'+tmp,end=',')
+            elif cnt==11:
+                print('mrt:'+tmp,end=',')
+            elif cnt==12:
+                print('na:'+tmp,end=',')
+            elif cnt==13:
+                print('afa:'+tmp,end=',')
+            elif cnt==14:
+                print('pinca:'+tmp,end=',')
+            else:
+                print('efc:'+tmp,end='},\n')
+            tmp=''
+            cnt+=1
+        elif v[i]=='"':
+            for j in range(i+1, len(v)):
+                if v[j]=='"':
+                    break
+                tmp+=v[j]
+            i=j
+        else:
+            tmp+=v[i]
+        i+=1
+print(']')
